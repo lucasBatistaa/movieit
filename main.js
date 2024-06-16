567fa8900c0d4b3265b123392205bb3442f3e246
lucasBatistaa <lucas.lucas2004.30@gmail.com>
2024-06-15 22:41:40 -0300

    create card of movies and comments

--- main.js
+++ main.js
@@ -1,23 +1,59 @@
 // https://www.omdbapi.com/?s=dune&type=movie&apikey=f474434e
 const API_TOKEN = 'f474434e'
 
-const loadMovies = async () => {
-    const main = document.querySelector('main')
+async function loadMovies() {
+    const highlightsMovies = document.querySelector('#highlights')
 
     try {
-        const response = await fetch(`https://www.omdbapi.com/?t=harry&apikey=f474434e`) 
+        const response = await fetch(`https://www.omdbapi.com/?s=game&type=movie&apikey=${API_TOKEN}`) 
         const data = await response.json()
         
-        console.log(data)
+        if (data.Search) {
+            const movies = data.Search.slice(0, 5)
+            
+            highlightsMovies.innerHTML = ''
 
-        main.innerHTML += `
-            <div> 
-                <h1>${data.Title}</h1>
-                <img src="${data.Poster}" />
-                <br>
-                <span>Nota: ${data.imdbRating}</span>
-            </div>
-        `
+            movies.forEach(movie => {
+                const movieElement = document.createElement('div')
+                
+                movieElement.classList.add('movie')
+                movieElement.id = movie.imdbID
+                
+                movieElement.innerHTML = `
+                    <img src="${movie.Poster}" alt="banner do filme" />
+                    
+                    <div class="movie-content">
+                        <h3>${movie.Title}</h3>
+                        
+                        <div class="movie-informations">
+                            <p>${movie.Year}</p>
+                            
+                            <div class="movie-avaliation">
+                                <img src="assets/star.svg" alt="Ícone de estrela das avaliações" />
+                                
+                                <p>8.8</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <a href="#" class="onmouse" style="display: none;">Ver mais</a>
+                `
+
+                const onMouseElement = movieElement.querySelector('.onmouse')
+
+                movieElement.addEventListener('mouseover', () => {
+                    onMouseElement.style.display = 'flex'
+                })
+
+                movieElement.addEventListener('mouseout', () => {
+                    onMouseElement.style.display = 'none'   
+                })
+
+                highlightsMovies.appendChild(movieElement)
+            })
+        } else {
+            console.log('Nenhum filme encontrado!')
+        }  
     } catch (error) {
         console.log(error)
     }
