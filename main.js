c1fc8d9a0fa9f605ce2445a7e730b82091dfc80e
lucasBatistaa <lucas.lucas2004.30@gmail.com>
2024-06-16 20:39:13 -0300

    create add and remove favorite movies functions

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-// https://www.omdbapi.com/?s=dune&type=movie&apikey=f474434e
 const API_TOKEN = 'f474434e'
+
+
+let FAVORITES_MOVIES = [
+    'tt0241527',
+    'tt0304141',
+    'tt1201607',
+    'tt1160419'
+]
+
+const MY_COMMENTS = [
+    {
+        id: 'tt0241527',
+
+    }
+]
 
 // Criar card do filme
 async function createCardMovie(movie) {
     const { Poster, Title, Type, Year, imdbID } = movie
 
+    console.log()
     const movieElement = document.createElement('div')
                 
     movieElement.classList.add('movie')
@@ -20,7 +35,7 @@ async function createCardMovie(movie) {
                 <p>${Year}</p>
                 
                     
-                <img class"icon-favorited" src="assets/heart.svg" alt="Ícone de favoritos" />
+                <img class"icon-favorited" src="${FAVORITES_MOVIES.includes(imdbID) ? 'assets/heart-fill.svg' : 'assets/heart.svg'}" alt="Ícone de favoritos" />
             </div>
         </div>
         
@@ -40,8 +55,41 @@ async function createCardMovie(movie) {
     return movieElement
 }
 
-// Carregar filmes
-async function loadMovies() {
+async function createCardFavorite(poster, title, imdbID) {
+
+    const movieElement = document.createElement('div')
+    movieElement.classList.add('card-favorite')
+    movieElement.setAttribute('movie-id', imdbID);
+
+    movieElement.innerHTML = `
+        <image src="${poster}" alt="banner do filme" />
+                    
+        <div class="card-favorite-content">
+            <div>
+                <h2>${title}</h2><span>(1999)</span>
+            </div>
+            
+            <p>Sci-fi, action</p>
+        </div>
+
+        <a id="${imdbID}" href="#" class="card-about-favorite" onclick="openModal(event.target.id)">Ver mais</a>
+    `
+
+    const onMouseElement = movieElement.querySelector('.card-about-favorite')
+
+    movieElement.addEventListener('mouseover', () => {
+        onMouseElement.style.display = 'flex'
+    })
+
+    movieElement.addEventListener('mouseout', () => {
+        onMouseElement.style.display = 'none'   
+    })
+
+    return movieElement
+}
+
+// Carregar filmes em destaque
+async function featuredMovies() {
     const highlightsMovies = document.querySelector('#highlights')
 
     try {
@@ -61,6 +109,24 @@ async function loadMovies() {
         } else {
             console.log('Nenhum filme encontrado!')
         }  
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Carregar filmes favoritos
+async function favoritedMovies() {
+    const favoritesMovies = document.getElementById('favorites-movies')
+
+    try {
+        for (const movieID of FAVORITES_MOVIES) {
+
+            const response = await fetch(`https://www.omdbapi.com/?i=${movieID}&type=movie&apikey=${API_TOKEN}`) 
+            const data = await response.json()
+    
+            const cardFavorite = await createCardFavorite(data.Poster, data.Title, movieID)
+            favoritesMovies.appendChild(cardFavorite)
+        }
     } catch (error) {
         console.log(error)
     }
@@ -114,27 +180,31 @@ async function dataMovieWithID(idMovie) {
 // Abrir Modal
 async function openModal(id) {
     const data = await dataMovieWithID(id)
-    console.log(data)
 
     const modal = document.getElementById("modal-movie-data")
-    
-    const poster = document.getElementById("modal-movie-poster")
-    const title = document.getElementById("modal-movie-title")
-    const year = document.getElementById("modal-movie-year")
-    const rating = document.getElementById("modal-movie-rating")
-    const duration = document.getElementById("modal-movie-duration")
-    const actors = document.getElementById("modal-movie-actors")
-    const directors = document.getElementById("modal-movie-directors")
-    const resume = document.getElementById("modal-movie-resume")
 
-    poster.src = data.Poster
-    title.innerHTML = data.Title
-    year.innerHTML = `(${(data.Year)})`
-    rating.innerHTML = data.imdbRating
-    duration.innerHTML = data.Runtime
-    actors.innerHTML = data.Actors
-    directors.innerHTML = data.Director
-    resume.innerHTML = data.Plot
+    modal.setAttribute('data-movie-id', id);
+
+    document.getElementById("modal-movie-poster").src  = data.Poster
+    document.getElementById("modal-movie-title").innerText = data.Title
+    document.getElementById("modal-movie-year").innerText = `(${(data.Year)})`
+    document.getElementById("modal-movie-rating").innerText = data.imdbRating
+    document.getElementById("modal-movie-duration").innerText = data.Runtime
+    document.getElementById("modal-movie-actors").innerText = data.Actors
+    document.getElementById("modal-movie-directors").innerText = data.Director
+    document.getElementById("modal-movie-resume").innerText = data.Plot
+
+    const buttonFavorite = document.getElementById("modal-button-favorite")
+
+    if (FAVORITES_MOVIES.includes(id)) {
+        
+        buttonFavorite.innerText = 'Desfavoritar'
+        buttonFavorite.onclick = removeToFavorites
+    } else {
+        
+        buttonFavorite.innerText = 'Favoritar'
+        buttonFavorite.onclick = addToFavorites
+    }
 
     modal.style.display = 'flex'
 }
@@ -147,7 +217,39 @@ async function closeModal(id) {
         modal.style.display = 'none'
     }
 }
+
+// Adicionar filme aos favoritos
+async function addToFavorites() {
+    const movieID = document.getElementById('modal-movie-data').getAttribute('data-movie-id')
+
+    const favoritesMovies = document.getElementById('favorites-movies')
+    const poster = document.getElementById("modal-movie-poster")
+    const title = document.getElementById("modal-movie-title")
+
+    const cardFavorite = await createCardFavorite(poster.src, title.textContent, movieID)
+    
+    favoritesMovies.appendChild(cardFavorite)
+    FAVORITES_MOVIES.push(movieID)
+}
+
+// Remover filme dos favoritos
+function removeToFavorites() {
+
+    const movieID = document.getElementById('modal-movie-data').getAttribute('data-movie-id')
+    const cardMovieFavorite = document.querySelector(`[movie-id="${movieID}"]`)
+
+    cardMovieFavorite.remove()
+
+    FAVORITES_MOVIES = FAVORITES_MOVIES.filter(id => id !== movieID)
+}
  
 // Adicionar EventListener no botão de pesquisa
 const searchButton = document.getElementById('searchButton')
 searchButton.addEventListener('click', searchMovie)
+
+// Carregar informações iniciais
+async function loadData() {
+    featuredMovies()
+
+    favoritedMovies()
+}
